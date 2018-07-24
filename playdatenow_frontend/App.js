9096da005505abc079b2165fedc44e43e167bf7a
cfalcant <cfalcant@gmail.com>
2018-07-24 13:32:58 -0700

    first component working

--- playdatenow_frontend/App.js
+++ playdatenow_frontend/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Home from './components/Home';
 
-export default class App extends React.Component {
+
+export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app</Text>
+        <Home></Home>
+        <Text>Open up App.js to start working on your app!!</Text>
         <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
       </View>
     );
   }
