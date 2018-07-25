49d242e149a20ebf6e1ac38cd108afac74965dd7
cfalcant <cfalcant@gmail.com>
2018-07-24 18:23:18 -0700

    added additional components.

---
+++ playdatenow_frontend/components/CreateNewPlaydate.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Text, View, StyleSheets } from 'react-native';
+
+export default class CreateNewPlaydate extends Component {
+    render () {
+        return (
+            <View>
+                <Text>
+                    Create New Playdate component
+                </Text>
+            </View>
+        )
+    }
+}
--- playdatenow_frontend/components/Home.js
+++ playdatenow_frontend/components/Home.js
@@ -2,19 +2,24 @@ import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import UserInfo from './UserInfo';
 import MyPlaydatesDash from './MyPlaydatesDash';
+import CreateNewPlaydate from './CreateNewPlaydate';
 
 export default class Home extends Component {
     render (){
         return (
             <View style = {{
                 flex: 1,
-                alignItems: 'center'    
+                alignItems: 'center',
+                marginTop: 20    
             }}>
-                < View style = {{flex: 1}}>
+                <View style = {{flex: 1}}>
                     <UserInfo/>
                 </View>
                 <View style = {{flex:1}}>
                     <MyPlaydatesDash/>
+                </View>
+                <View style = {{flex: 1}}>
+                    <CreateNewPlaydate/>
                 </View>
             </View>
         )
