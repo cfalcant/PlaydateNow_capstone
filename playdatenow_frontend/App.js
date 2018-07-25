03b310eef46c991a26bafe94e852670b5db37b44
cfalcant <cfalcant@gmail.com>
2018-07-24 23:15:33 -0700

    created Playdates screen

--- playdatenow_frontend/App.js
+++ playdatenow_frontend/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Home from './components/Home';
-
+import Playdates from './components/Playdates';
 
 
 export default class App extends Component {
@@ -10,6 +10,7 @@ export default class App extends Component {
       <View style={styles.container}>
         <View style={{flex:1}}>
           <Home></Home>
+          {/* <Playdates></Playdates> */}
         </View>
       </View>
     );
---
+++ playdatenow_frontend/components/Playdates.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default class Playdates extends Component {
+    render () {
+        return (
+            <View>
+            <Text>
+                Playdates component
+            </Text>
+            </View>
+        )
+    }
+}
