c19e4ce1083e2bb29821d2413031425b6837317e
cfalcant <cfalcant@gmail.com>
2018-07-27 14:27:03 -0700

    Working

--- playdatenow_frontend/config/Router.js
+++ playdatenow_frontend/config/Router.js
@@ -1,13 +1,2 @@
-import React from 'react'
-import { Scene, Router, Actions } from 'react-native-router-flux'
-import { View } from 'react-native'
 
-import Home from '../components/Home'
-import Playdates from '../components/Playdates'
-import Settings from '../components/Settings'
-
-const RouterComponent = () => {
-
-    
-}
 
