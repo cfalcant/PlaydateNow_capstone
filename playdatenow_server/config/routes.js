27e157e5037895a0f6fa13e0cac49e4196162ecb
cfalcant <cfalcant@gmail.com>
2018-07-27 20:35:10 -0700

    users CRUD working

--- playdatenow_server/config/routes.js
+++ playdatenow_server/config/routes.js
@@ -2,8 +2,10 @@
 const users = require("../controllers/users.js")
 module.exports = function (app) {
 
-    app.get('/', users.index);
-
+    app.get('/users', users.index);
+    app.get('/users/:id', users.getOne);
     app.post('/users', users.create);
+    app.put('/users/:id', users.update);
+    app.delete('/users/:id', users.delete);
 
 }
--- playdatenow_server/controllers/users.js
+++ playdatenow_server/controllers/users.js
@@ -2,10 +2,6 @@ const knex = require("../db/knex.js");
 
 module.exports = {
     // CHANGE ME TO AN ACTUAL FUNCTION
-    index: function (req, res) {
-        res.send("Hello");
-    },
-
     create: function (req, res) {
         knex('users').insert({
             firstname: req.body.firstname,
@@ -17,5 +13,31 @@ module.exports = {
             res.json(true);
         })
     },
+    index: (req,res)=>{
+        knex('users').then((results)=>{
+            res.json(results);
+        })
+    },
+    getOne: (req,res)=> {
+        knex('users').where('id', req.params.id).then((results)=>{
+            res.json(results[0]);
+        })
+    },
+    delete: (req,res)=>{
+        knex('users').delete().where('id', req.params.id).then(()=>{
+            res.json(true);
+        })   
+    },
+    update: (req,res)=>{
+        knex('users').update({
+            firstname: req.body.firstname,
+            lastname: req.body.lastname,
+            email: req.body.email,
+            password: req.body.password,
+            zipcode: req.body.zipcode
+        }).where('id', req.params.id).then(()=>{
+            res.json(true);
+        })
+    }
 
 }
