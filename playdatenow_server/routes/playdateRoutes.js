257c7fdc6553bc218a302e76bbf2573a33bdd91d
cfalcant <cfalcant@gmail.com>
2018-07-30 18:11:50 -0700

    working

--- playdatenow_server/routes/playdateRoutes.js
+++ playdatenow_server/routes/playdateRoutes.js
@@ -7,6 +7,14 @@ router.get('/', function (req, res, next) {
     knex('playdates').then(playdates => res.json(playdates))
 });
 
+/*GET a SINGLE playdate using ID */
+router.get('/:id', function (req, res) {
+    knex('playdates')
+        .select()
+        .where('id', req.params.id)
+        .then(playdate => res.json(playdate[0]));
+});
+
 /*POST new playdate*/
 router.post('/', function (req, res) {
     knex('playdates')
