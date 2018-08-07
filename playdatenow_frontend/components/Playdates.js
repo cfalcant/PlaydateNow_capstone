5f18f7cf87807d3ccf61f7af38f5e68d38b24782
cfalcant <cfalcant@gmail.com>
2018-08-07 12:01:30 -0700

    Fixed search screen

--- playdatenow_frontend/components/Playdates.js
+++ playdatenow_frontend/components/Playdates.js
@@ -11,8 +11,18 @@ state = {
 
 render () {
     return (
-        <View style = {styles.screenContainer}>
-            <View style={styles.topContainer}>
+        < View style = {{
+                // flex: 1,
+                justifyContent: 'center',
+                alignItems: 'stretch',
+                // padding: 10
+            }}>
+            < View style = {{
+                    // flex: 1,
+                    justifyContent: 'space-around',
+                    backgroundColor: '#c997fc',
+                    padding: 10
+                }} >
                 <Text style={{fontSize: 25}}>
                 Search by zip code:
                 </Text>
@@ -46,12 +56,24 @@ render () {
             </View>
 
             
-            <View style={styles.bottomContainer}>
+            < View style = {{
+                    // flex: 5,
+                    justifyContent: 'space-around',
+                    backgroundColor: '#7b51a5',
+                    padding: 10
+                }}>
                 <Text style={{color: 'white', fontSize: 25}}>
                     Search by category:
                 </Text>  
 
-                    <View style={{flex: 3,position: 'relative', bottom: 0, right: 0, left: 0, top: 10}}>
+                    <View style={{
+                        // flex: 3,
+                        flex: 2,
+                        position: 'absolute',
+                        bottom: 0,
+                        right: 0,
+                        left: 0,
+                        top: 50}}>
                         <Picker
                             
                             selectedValue = {this.state.language}
@@ -85,15 +107,24 @@ render () {
                         </Picker>
                     </View>
 
-                    <View style={{flex: .9}}>
+                    <View style={{flex: .9, paddingTop: 20}}>
                         <TouchableHighlight
-                            style = {styles.btn}
+                            style = {{
+                                    padding: 10,
+                                    paddingTop: 10,
+                                    backgroundColor: '#898988',
+                                    // backgroundColor: 'red',
+                                    alignItems: 'center',
+                                    borderRadius: 5,
+                                    position: 'relative',
+                                    height: 50
+                            }}
                             // style = {{backgroundColor: 'red'}}
                             onPress = {() => {Actions.SearchResults();}}
                             underlayColor = 'white'
                             // value={this.state.zipcode}    
                         >
-                            <Text style = {{fontSize: 25}}>
+                            <Text style = {{fontSize: 25, color: 'black'}}>
                                 Search 
                             </Text>  
                         </TouchableHighlight>
@@ -109,24 +140,24 @@ render () {
 }
 
 styles = StyleSheet.create({
-    screenContainer: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'stretch',
-        padding: 10
-    },
-    topContainer:{
-            flex: 1,
-            justifyContent: 'space-around',
-            backgroundColor: '#c997fc',
-            padding: 10
-    },
-    bottomContainer: {
-            flex: 1, 
-            justifyContent: 'space-around',
-            backgroundColor: '#7b51a5',
-            padding: 10
-    },
+    // screenContainer: {
+    //     flex: 1,
+    //     justifyContent: 'center',
+    //     alignItems: 'stretch',
+    //     padding: 10
+    // },
+    // topContainer:{
+    //         flex: 1,
+    //         justifyContent: 'space-around',
+    //         backgroundColor: '#c997fc',
+    //         padding: 10
+    // },
+    // bottomContainer: {
+    //         flex: 1, 
+    //         justifyContent: 'space-around',
+    //         backgroundColor: '#7b51a5',
+    //         padding: 10
+    // },
     btn: {
         padding: 10,
         // backgroundColor: '#898988',
